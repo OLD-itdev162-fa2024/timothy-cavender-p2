c4ff6b139749cf5a3a4fd0f1edbb6beeaa8b03d8
unknown <cavendert@gmail.com>
2023-11-27 15:49:51 -0600

    Layout for Fridge.html

--- client/src/app/fridge/fridge.component.ts
+++ client/src/app/fridge/fridge.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-fridge',
   templateUrl: './fridge.component.html',
   styleUrls: ['./fridge.component.css']
 })
+
+
 export class FridgeComponent implements OnInit {
 
-  constructor() { }
+  /*Import this from database.  */
+  ingredients: any = [
+    {name: "Dairy", value: [
+      {name: "Milk", value: 5},
+      {name: "Cheese", value: 3}
+      ]
+    },
+    {name: "Produce", value: [
+      {name: "Tomatoe", value: 5},
+      {name: "Brocoli", value: 6}    
+      ]
+    }
+    
+  ];
+  
+  constructor() {}
 
   ngOnInit(): void {
+    //Will need to pull food items/ingredients from database
+    //to populate in the Fridge HTML screen
+
+    //Will also need to add/remove ingredients
   }
 
 }
