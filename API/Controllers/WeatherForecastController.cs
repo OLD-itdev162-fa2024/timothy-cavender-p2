82d30d9d489e7d1691a5cb82862f490e870d1231
unknown <cavendert@gmail.com>
2023-11-29 15:59:30 -0600

    Initial API Set

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -1,3 +1,5 @@
+using Domain;
+using Persistence;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
@@ -13,9 +15,12 @@ public class WeatherForecastController : ControllerBase
 
     private readonly ILogger<WeatherForecastController> _logger;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    private readonly DataContext _context;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, DataContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
@@ -28,5 +33,30 @@ public class WeatherForecastController : ControllerBase
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
         })
         .ToArray();
+    }
+
+    [HttpPost]
+    public ActionResult<WeatherForecast> Create()
+    {
+        //View in Console
+        Console.WriteLine($"Database path: {_context.DbPath}");
+        Console.WriteLine("Insert a new weather forecast");
+
+        var forecast = new WeatherForecast() 
+        {
+            Date = new DateOnly(),
+            TemperatureC = 75,
+            Summary = "Warm"
+        };
+
+        _context.WeatherForecasts.Add(forecast);
+        var success = _context.SaveChanges() > 0;
+
+        if(success)
+        {
+            return forecast;
+        }
+
+        throw new Exception("Error Creating Weather Forecast");
     }
 }
